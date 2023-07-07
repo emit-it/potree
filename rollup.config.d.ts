99d8f3b104a53a5eef4f24dd88f78d6db5cf0c91
Aron Soppe <a.soppe@emit-it.com>
2023-07-07 15:49:37 +0200

    feat: adds typescript emitting type declarations (still breaks after running `tsc`)

---
+++ rollup.config.d.ts
@@ -0,0 +1,20 @@
+declare const _default: ({
+    input: string;
+    treeshake: boolean;
+    output: {
+        file: string;
+        format: string;
+        name: string;
+        sourcemap: boolean;
+    };
+} | {
+    input: string;
+    output: {
+        file: string;
+        format: string;
+        name: string;
+        sourcemap: boolean;
+    };
+    treeshake?: undefined;
+})[];
+export default _default;
